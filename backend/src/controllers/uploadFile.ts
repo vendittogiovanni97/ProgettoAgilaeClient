fc02df9a75d99209a1d774fed7569d3914bb86c4
VendittoGiovanni <vendittogiovanni97@hotmail.it>
2025-04-24 17:44:17 +0200

    Inizializzazione del backend per upload file, + file git e ricerca di materiale per i file

---
+++ backend/src/controllers/uploadFile.ts
@@ -0,0 +1,47 @@
+import { UploadedFile } from "express-fileupload";
+import { Request, Response } from "express";
+import path from "path";
+import dbClient from "../configuration/db.config";
+
+export const uploadProfilePic = async (
+  request: Request,
+  response: Response
+) => {
+  const profilePicsPath = "./public/profilePics";
+  const files = Array.isArray(request.files?.profilePic)
+    ? request.files?.profilePic
+    : [request.files?.profilePic];
+
+  console.log(request.body.mioJson);
+
+  if (files.length > 1) {
+    response.status(400).json("You can upload one file only");
+    return;
+  }
+
+  const file: UploadedFile | undefined = files[0];
+
+  if (file === undefined) {
+    response.status(400).json("Provide a profilePic");
+    return;
+  }
+
+  if (!file.mimetype.startsWith("image/")) {
+    response.status(400).json("Invalid file type");
+    return;
+  }
+
+  const ext = path.extname(file.name);
+
+  const filename = `${file.name}-${new Date().valueOf()}${ext}`;
+
+  file.mv(`${profilePicsPath}/${filename}`);
+
+  await dbClient.profile.update({
+    data: {
+      profilePic: filename,
+    },
+  });
+
+  response.status(200).json({ filename });
+};
---
+++ backend/src/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+if (process.env.SESSION_SECRET === undefined) {
+  throw new Error("Define SESSION_SECRET");
+}
+
+const app = express();
+
+app.use((request, response, next) => {
+  console.log(request.method, request.url);
+  next();
+});
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta ${port}`);
+});
